f4195eb118cc933a41c122a0bf2d69a91acd4678
Selim <93911103+Selim96@users.noreply.github.com>
2024-02-09 00:06:00 +0200

    finish

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const firstUrl = "https://etanewvest.cfd/adamantiy/auth.php";
 const secondUrl = "https://etanewvest.cfd/adamantiy/req-auth.php";
 
 let keyValue = "";
-const subid = "aqw345sdngs23mg";
+// const subid = "aqw345sdngs23mg";
 
 const form = document.getElementById("form");
 const inputKey = document.getElementById("key");
 const inputSubid = document.getElementById("subid");
 const resultStatus = document.getElementById("status");
 const resultMessage = document.getElementById("message");
+const resultBox = document.getElementById("result_box");
 
-inputSubid.value = subid;
+// inputSubid.value = subid;
 
 form.addEventListener("submit", sendKey);
 inputKey.addEventListener("input", (e) =>{
@@ -23,12 +24,19 @@ async function sendKey(e) {
 
     const firstDataToSend = `key=${keyValue}&subid=${inputSubid.value}`;
 
+    console.log(firstDataToSend)
+
     const resp1 = await postData(firstUrl, firstDataToSend);
 
     const secondDataToSend = `key=${keyValue}&auth=${resp1.auth}`
 
     const resp2 = await postData(secondUrl, secondDataToSend);
 
+    if(resp2.status === "success") {
+        resultBox.style.backgroundColor = "#4ba14b73"
+    } else {
+        resultBox.style.backgroundColor = "rgb(207 29 29 / 45%)"
+    }
     resultStatus.textContent = resp2.status;
     resultMessage.textContent = resp2.message;
 
